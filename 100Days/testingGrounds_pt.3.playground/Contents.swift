a19aae8269c0291321fa14b8ed247d7f9fbfecf0
Alexander McGreevy <150959413+REDACTED22@users.noreply.github.com>
2025-02-10 18:53:28 -0500

    Day 10 complete

--- 100Days/testingGrounds_pt.3.playground/Contents.swift
+++ 100Days/testingGrounds_pt.3.playground/Contents.swift
@@ -37,3 +37,114 @@ doImportantWork {
 } third: {
     print("This is the third work")
 }
+
+//Structs(Swifts version of objects)
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+
+//functions need to be mutating in order to change the variables
+struct Employee {
+    let name: String
+    var vacationRemaining: Int
+
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+//The struct can be made to use data more dynamically
+struct Employee2 {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+//getter and setter dynamically modifys var
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var archer2 = Employee2(name: "Sterling Archer", vacationAllocated: 14)
+archer2.vacationTaken += 4
+archer2.vacationRemaining = 5//will change the other vaariables to match this
+print(archer2.vacationAllocated)//5+4=9(initial value for allocatd is overwritten)
+
+//Property observers
+struct Game {
+    var score = 0 {
+        didSet {//didSet activates when the variable is altered
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+//didSet and willSet can be used along with newValue snd oldValue
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+
+//Use init to initialize class properties
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
