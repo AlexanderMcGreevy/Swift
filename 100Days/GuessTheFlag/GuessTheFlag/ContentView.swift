cdf72de06328bfbc469d07981a246f2bee8d4fa6
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-02-27 14:40:16 -0500

    Guess the Flag has been finished

--- 100Days/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100Days/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,21 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
+    //Var space
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var flagPressed = 0
 
-            Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
+    
+    var body: some View {
+            ZStack{
+                AngularGradient(colors: [.red, .black, .indigo, .indigo, .black, .red], center: UnitPoint(x: 0.5, y: 0.55))
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Text("Guess the Flag")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundStyle(.white)
+                        .padding()
+                          
+
+                    VStack(spacing: 25) {
+                        VStack {
+                            Text("Tap the flag of").foregroundStyle(.white).font(.subheadline.weight(.heavy))
+
+                            Text(countries[correctAnswer]).foregroundStyle(.white).font(.largeTitle.weight(.semibold))
+
+                        }
+
+                        ForEach(0..<3) { number in
+                            Button {
+                                flagTapped(number)
+                                flagPressed=number
+                            } label: {
+                                Image(countries[number]).clipShape(RoundedRectangle(cornerRadius: 20)).shadow(color: .black, radius: 6)
+
+                            }
+                        }
+                    }.frame(maxWidth: 250)
+                        .padding(.vertical, 15)
+                        .background(Color.black.opacity(0.3))
+                        .clipShape(.rect(cornerRadius: 20))
+                        .padding()
+                        .padding()
+
+                    Text("Score: \(score)")
+                        .foregroundStyle(.white)
+                        .font(.title.bold())
+                        .padding()
+
+                }
+                
+                
+            }.alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("That's the flag of \(countries[flagPressed])!")
+            }
+            
         }
-        .ignoresSafeArea()
+    //Function space
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            let tap = UIImpactFeedbackGenerator(style: .soft)
+            tap.impactOccurred()
+            score+=1
+
+            askQuestion()
+        } else {
+            scoreTitle = "Wrong"
+            let impact = UIImpactFeedbackGenerator(style: .rigid)
+                impact.impactOccurred()
+            showingScore = true
+        }
     }
-}
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    }
+
+        
+    
+
 
 #Preview {
     ContentView()
