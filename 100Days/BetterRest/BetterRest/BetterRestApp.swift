9892a26978acac0edee4944eb5f535de831f5baa
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-03-05 17:26:06 -0500

    Started work on BetterRest

---
+++ 100Days/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alexander McGreevy on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
