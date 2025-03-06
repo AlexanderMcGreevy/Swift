48f6083d242fcf19edb200d894ebd2322530a346
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-03-06 11:22:37 -0500

    Continuing work on BetterSleep

--- 100Days/BetterRest/BetterRest/ContentView.swift
+++ 100Days/BetterRest/BetterRest/ContentView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
-    
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
 
     
     
     var body: some View {
         VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-                .labelsHidden()
+            Text("When do you want to wake up?")
+                        .font(.headline)
 
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+
+            Text("Desired amount of sleep")
+                .font(.headline)
+
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             
-        }.padding()
+            Text("Daily coffee intake")
+                .font(.headline)
+
+            Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+            
+        }.padding().navigationTitle("BetterRest").toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        
+        
+    }
+    func calculateBedtime() {
     }
 }
 
