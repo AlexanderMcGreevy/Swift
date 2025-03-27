65234cf17ecf2887c80f6e204010de5aa675cc54
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-03-27 14:56:01 -0400

    Start of Moonshot project

---
+++ 100Days/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alexander McGreevy on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
