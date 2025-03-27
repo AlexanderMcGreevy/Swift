7512f59264035841a2c9187b5b566752519ae856
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-03-27 18:20:02 -0400

    First testing done

--- 100Days/Moonshot/Moonshot/ContentView.swift
+++ 100Days/Moonshot/Moonshot/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let layout = [
+            GridItem(.adaptive(minimum: 80, maximum: 120)),
+        ]
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
