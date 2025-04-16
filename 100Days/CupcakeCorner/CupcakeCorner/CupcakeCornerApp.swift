9ec84264d8bf5c1a62fd4caa4c56faab722f1403
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-04-16 13:36:35 -0400

    Started Cupcake corner
    App for tracking itunes songs

---
+++ 100Days/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alexander McGreevy on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100Days/CupcakeCorner/CupcakeCornerTests/CupcakeCornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerTests.swift
+//  CupcakeCornerTests
+//
+//  Created by Alexander McGreevy on 4/16/25.
+//
+
+import Testing
+@testable import CupcakeCorner
+
+struct CupcakeCornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
