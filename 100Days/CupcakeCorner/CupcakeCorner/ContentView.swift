5b4f853328c0caf9478d29573aa040d3279801ee
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-04-17 10:35:31 -0400

    figured out asyncronous image calling

--- 100Days/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ 100Days/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,48 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
 
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        var disableForm: Bool {
+            username.count < 5 || email.count < 5
+        }
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        }.task {
-            await loadData()
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }.disabled(disableForm)
+
+
         }
     }
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=the+strokes&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-
-
-    }
 }
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 
 
 #Preview {
