c5a6aa9e7b5707017b95216d1a518a6ed678a193
Alexander McGreevy <150959413+AlexanderMcGreevy@users.noreply.github.com>
2025-09-05 18:51:23 -0400

    RankFlix Beginning

---
+++ RankFlix/RankFlix/RankFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  RankFlixApp.swift
+//  RankFlix
+//
+//  Created by Alexander McGreevy on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RankFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RankFlix/RankFlixTests/RankFlixTests.swift
@@ -0,0 +1,17 @@
+//
+//  RankFlixTests.swift
+//  RankFlixTests
+//
+//  Created by Alexander McGreevy on 9/5/25.
+//
+
+import Testing
+@testable import RankFlix
+
+struct RankFlixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
